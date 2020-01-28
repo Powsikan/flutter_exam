a41e7bddbe885e61f96c81d7d5a8d525b1be09c8
powsokan <powsi1997@gmail.com>
2020-01-28 18:05:21 +0530

    bottom navigation added

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -10,6 +10,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueAccent.shade200,
         title: Text("Call"),
         centerTitle: true,
         actions: <Widget>[
@@ -20,15 +21,41 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      body: Container(
-        child: ListView(),
+      body: Center(
+
+        child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(10.0)),
+            Text(
+              "Nabeel",
+              style: TextStyle(color: Colors.red),
+            ),
+            Padding(padding: EdgeInsets.all(10.0)),
+            Text(
+              "Minsha",
+              style: TextStyle(color: Colors.red),
+            ),
+            Padding(padding: EdgeInsets.all(10.0)),
+            Text(
+              "Faheem",
+              style: TextStyle(color: Colors.red),
+            ),
+            Padding(padding: EdgeInsets.all(10.0)),
+            Text(
+              "Kesavan",
+              style: TextStyle(color: Colors.red),
+            ),
+            Padding(padding: EdgeInsets.all(10.0)),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.call_made),title:Text("Make Call")),
-        BottomNavigationBarItem(icon: Icon(Icons.call_end),title:Text("End Call")),
-        BottomNavigationBarItem(icon: Icon(Icons.call_missed),title:Text("Missed Call")),
-
-
+        BottomNavigationBarItem(
+            icon: Icon(Icons.call_made), title: Text("Make Call")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.call_end), title: Text("End Call")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.call_missed), title: Text("Missed Call")),
       ]),
     );
   }
